ab511cf505299af9b5e8aa3e8aa80f94a01e6ec8
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-30 10:04:49 -0300

    Create _atividade_16.java

---
+++ _atividade_16.java
@@ -0,0 +1,16 @@
+package Java_Lista_7_Faca_Enquanto;
+
+public class _atividade_16 {
+    public static void main(String[] args) {
+        int mp, minus;
+
+        minus = 1;
+        mp = 101;
+
+        do {
+            mp=mp-minus;
+            System.out.println("a quantidade de materia prima usada é :"+mp);
+        }while (mp>10);
+        System.out.println("bom trabalho");
+    }
+}
